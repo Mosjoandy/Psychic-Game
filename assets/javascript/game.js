8c0dc18b65de2315f7e8ac7de14f05dc5828c432
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-04-24 18:23:54 -0700

    added guesses for all if, added total numbers and display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,50 @@
-var guessTotalShow = document.getElementById("guessTotalShow");
-var lossTotalShow = document.getElementById("lossTotalShow");
-var winTotalShow = document.getElementById("winTotalShow");
-
 var cpuRandom = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var humanInput = "";
 var guessTotal = 9;
 var lossTotal = 0;
 var winTotal = 0;
+var humanAttempts = [];
 
 cpuGuess = cpuRandom[Math.floor(Math.random() * cpuRandom.length)];
         console.log("Computer pressed: " + cpuGuess);
 
 document.onkeyup = function(humanGuess) {
     humanInput = humanGuess.key;
-    console.log("You pressed: " + humanInput);   
+    console.log("You pressed: " + humanInput);  
+ 
 
         if (humanInput != cpuGuess) {
             guessTotal = (guessTotal-1);
-            console.log(guessTotal);
+            console.log("You have " + guessTotal +" left");
             document.getElementById("guessTotalShow").innerHTML = (guessTotal);
+            humanAttempts.push(humanInput);
+            document.getElementById("humanGuesses").innerHTML = (humanAttempts);
 
+            
             if (guessTotal === 0) {
                 lossTotal = (lossTotal+1);
                 console.log("increasetheloss");
                 document.getElementById("lossTotalShow").innerHTML = (lossTotal);
-                guessTotal = (guessTotal+9);
+                guessTotal = 9;
+
+                humanAttempts = [];
+                document.getElementById("humanGuesses").innerHTML = humanAttempts;
             }
+
+            
         }
 
-        if (humanInput === cpuGuess) {
+        else if (humanInput === cpuGuess) {
             winTotal = (winTotal+1);
             console.log("increasethewin");
             document.getElementById("winTotalShow").innerHTML = (winTotal);
-            guessTotal = (guessTotal+9);
+            guessTotal = (guessTotal=9);
+
+            humanAttempts = [];
+            document.getElementById("humanGuesses").innerHTML = humanAttempts;
+
+            cpuGuess = cpuRandom[Math.floor(Math.random() * cpuRandom.length)];
+            console.log("Computer pressed: " + cpuGuess);
         }
 }
 
